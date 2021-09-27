518e41973fad6a5894cccddd497e7b967dbb362b
Claudi4 <clau.ale.20a@gmail.com>
2021-09-27 16:39:08 -0500

    find file md

---
+++ lib/library/libraryTree.md/fileTreeOne.md
@@ -0,0 +1,2 @@
+## 1. HOLI BOLI SOY 3.1
+[Píldora recursión - YouTube Laboratoria Developers](https://www.youtube.com/watch?v=lPPgY3HLlhQ)
--- src/api.js
+++ src/api.js
@@ -2,25 +2,60 @@
 /*module.exports = () => {
     // ...
   };*/
-  let prueba = process.argv.slice(2);
-  console.log(prueba,6);
 
+  let prueba = process.argv.slice(2);
+  console.log(prueba,7);
+
+  const pruebaPath = {
+    relativa: 'ib',
+    absoluta: 'C:/Users/Laboratoria/Documents/GitHub/LIM015-md-links/lib',
+    fileAndMd: 'C:/Users/Laboratoria/Documents/GitHub/LIM015-md-links/lib/fileOne.md'
+  }
   const path = require('path')
   const fs = require('fs');
-
-  const isAbsolute = (file) => (path.isAbsolute(file) ? file : path.resolve(file));
-  console.log(isAbsolute('README.md'),12);
-
-  const  isExit = (rote) => fs.existsSync(rote);
-  console.log(isExit('C://Users//Laboratoria//Documents//GitHub//LIM015-md-links//README.md'),15);
-
   
+  const isAbsolute = (file) => (path.isAbsolute(file) ? file : path.resolve(file));//if
+
+  const  isExit = (route) => fs.existsSync(route);
+  
+  const isDirectory =(route) => fs.lstatSync(route).isDirectory(); 
+  
+  const isFile =(route) => fs.lstatSync(route).isFile(); 
+  
+  const isFileMd = (route) => path.extname(route) === '.md';
+  
+  const readDirectory =(route) => fs.readdirSync(route);
+
+  console.log('convert absolute a relative y devuelve absolute:',isAbsolute(pruebaPath.relativa)); //true Ruta
+  console.log('exit path:',isExit(pruebaPath.absoluta));//true
+  console.log( 'is directory :',isDirectory(pruebaPath.absoluta)); //true
+  console.log( 'is file:',isFile(pruebaPath.fileAndMd));//true
+  console.log( 'the Flile is Md :',isFileMd(pruebaPath.fileAndMd)); //true
+  console.log( 'read directory:',readDirectory(pruebaPath.absoluta));//true
 
 
+//LEE ARCHIVOS MD
+const arrayOfFiles = [];
+const getFilesMd = (route) => {
+  if(isDirectory(route)) {
+    console.log(route, '---> Es una carpeta');
+    const directoryOfArchives = readDirectory(route);
+    console.log(directoryOfArchives,107);
+    directoryOfArchives.forEach((elem) => {
+      const routeElem = elem;
+      const newRoute = path.join(route, routeElem);
+      getFilesMd(newRoute);;
+      
+  })} else if(isFileMd(route)){
+    arrayOfFiles.push(route);
+  };
+  return arrayOfFiles;
+};
+console.log('El archivo MD es ---> ', getFilesMd(pruebaPath.absoluta));
 
 
   module.exports = {
-    prueba:prueba,
-    isAbsolute:isAbsolute,
-    isExit:isExit,
+    prueba,
+    isAbsolute,
+    isExit,
   };
